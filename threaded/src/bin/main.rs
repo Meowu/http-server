f55a495f8be14a920992105734a67b52c4640dee
Meowu <474384902@qq.com>
2024-12-31 22:00:27 +0800

    impl threaded server.

--- threaded/src/bin/main.rs
+++ threaded/src/bin/main.rs
@@ -4,15 +4,19 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
+use threaded::ThreadPool;
 
 fn main() {
     let address = "127.0.0.1:7878";
     let listener = TcpListener::bind(address).unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        })
     }
 }
 
